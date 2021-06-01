e0d34ac242a769b405c91fa850115647f97ec6f0
Andre Lima <andre.p.lima@gmail.com>
2021-06-01 17:27:49 -0300

    working on sims2

---
+++ sims2.py
@@ -0,0 +1,28 @@
+def main():
+
+	# determines the simulation parameters
+	m
+	n
+	d
+
+
+	# loads the 'Spotify's Worldwide Daily Song Ranking' (WDSR) dataset
+	# -- only records associated to the target population (associated to particular region) are considered
+	# -- URL terminal node is used as song identifier (WSDR.id)
+	# -- timeline, mostPopularSongs
+
+	# loads the 'Spotify Dataset 1922-2021, ~600k tracks' (D600k) dataset
+	# -- premise [P2] The selected audio-features are Tversky-relevant for users.
+
+	# builds the relationship between the datasets
+	# -- xxx is a map between WDSR.id and D600k.id
+
+	# determines the convex hull that encloses all popular items in the target population
+	# -- premise [P1] Most users have popular items in their profile
+	for date in timeline:
+
+
+
+if __name__ == "__main__":
+
+	main()
